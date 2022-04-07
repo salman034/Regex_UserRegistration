467b0fdc59000dd86774810a10339e9b0ece4a1e
unknown <you@example.com>
2022-04-07 15:59:44 +0530

    Added: Problems Based On Regular Experation

---
+++ src/com/bl/userregistration/Main.java
@@ -0,0 +1,7 @@
+package com.bl.userregistration;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The User Registration Problem!");
+    }
+}
