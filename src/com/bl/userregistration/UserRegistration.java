d4b5e7d37d4f3e31aae979f45601ffc39702213b
unknown <you@example.com>
2022-04-08 06:45:18 +0530

    Added:User need to enter a valid Last Name

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-    public boolean firstName() {
-        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "Salman");
+    public boolean lastName() {
+        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "Pasha");
         return matchesString;
     }
 }
