182feb55e4e315642a9a4888fb4f7ac1c6bb8a72
unknown <you@example.com>
2022-04-08 07:17:49 +0530

    Added: User need to enter a valid Password rules minimum 8 characters

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-    public boolean phoneNumber() {
-        boolean matchesString = Pattern.matches("^[0-9]{2}[ ][789]{1}[0-9]{9}$","91 9845715264");
+    public boolean password() {
+        boolean matchesString = Pattern.matches("^[a-z]{8}$","mkspasha");
         return matchesString;
     }
 }
