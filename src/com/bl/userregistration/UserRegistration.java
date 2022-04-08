3547185a5f052104b9f0c7c05b4dab60f3d55e73
unknown <you@example.com>
2022-04-08 07:04:16 +0530

    Added: User need to enter a valid PhoneNumber

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -4,9 +4,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-    public boolean email() {
-        boolean matchesString = Pattern.matches("^[A-Za-z._]+[@]{1}[a-zA-Z]+[.a-z]{4}([.a-zA-Z]{3})*$",
-                                                   "mkspasha.xyz@gmail.com");
+    public boolean phoneNumber() {
+        boolean matchesString = Pattern.matches("^[0-9]{2}[ ][789]{1}[0-9]{9}$","91 9845715264");
         return matchesString;
     }
 }
