38f5dbc05628f40ecc9c5a6b945b12fe0f09b25b
unknown <you@example.com>
2022-04-09 06:42:30 +0530

    Added: User need to enter atleast 1 Upper Case

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-    public boolean password() {
-        boolean matchesString = Pattern.matches("^[a-z]{8}$","mkspasha");
+    public boolean passwordRule2() {
+        boolean matchesString = Pattern.matches("^[A-Z{1}a-z]{8}$","Mkspasha");
         return matchesString;
     }
 }
