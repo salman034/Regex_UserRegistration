33e7518a33ff29f94d4bd392c87a19718bc37a9a
unknown <you@example.com>
2022-04-09 06:53:35 +0530

    Added: Should have atleast 1 Numeric number in the Password

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-    public boolean passwordRule2() {
-        boolean matchesString = Pattern.matches("^[A-Z{1}a-z]{8}$","Mkspasha");
+    public boolean passwordRule3() {
+        boolean matchesString = Pattern.matches("^[A-Z{1}0-9{1}a-z]{8}$", "Sa9amash");
         return matchesString;
     }
 }
--- src/com/bl/userregistration/UserRegistrationTest.java
+++ src/com/bl/userregistration/UserRegistrationTest.java
@@ -7,9 +7,9 @@ public class UserRegistrationTest {
     private Assertions Assert;
 
     @Test
-    public void givenPasswordWhenUpperCaseShouldReturnTrue() {
+    public void givenPasswordWhenOneNumericShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.passwordRule2();
+        boolean userTest = userRegistration.passwordRule3();
         System.out.println(userTest);
         Assert.assertTrue(userTest);
     }
