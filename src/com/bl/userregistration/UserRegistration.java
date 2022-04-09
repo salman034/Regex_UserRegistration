faecee46c10b7e62eefba3a54c539d8efc9777cf
unknown <you@example.com>
2022-04-09 07:09:22 +0530

    Added: Should clear all Email samples provided separately

--- src/com/bl/userregistration/UserRegistration.java
+++ src/com/bl/userregistration/UserRegistration.java
@@ -4,8 +4,10 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class UserRegistration {
-    public boolean passwordRule4() {
-        boolean matchesString = Pattern.matches("^([~!@#$%^&*()+{}]?[A-Za-z0-9]){8,}$", "Salman@7744");
+    public static final String EMAIL_TESTS = "^[a-zA-Z0-9+_.-]+[@][a-zA-Z0-9]+[.a-z]{4}([.a-zA-Z,]{3})*$";
+
+    public boolean emailTest(String email) {
+        boolean matchesString = Pattern.matches(EMAIL_TESTS, email);
         return matchesString;
     }
 }
